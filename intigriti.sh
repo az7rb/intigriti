087b6463a16707a34008a3abafe7fb5bcf7ae8e4
az7rb <54269260+az7rb@users.noreply.github.com>
2022-06-08 12:42:42 +0300

    Update intigriti.sh

--- intigriti.sh
+++ intigriti.sh
@@ -21,13 +21,18 @@ curl -s "https://app.intigriti.com/profile/$1" | grep "Country" | cut -d'>' -f11
 	
 curl -s "https://app.intigriti.com/profile/$1" | grep "Twitter" | cut -d'>' -f30 | cut -d '=' -f4 | cut -d '"' -f2 | awk  '{print "[-] Twitter: "  $0}'
 
-#Rank()
-
-curl -s "https://app.intigriti.com/profile/$1" | grep "Rank"| awk -F# '{print "[-] My Rank in Intigriti.com: "  $2}' | cut -d'<' -f1
-
 #Streak()
 
 curl -s "https://app.intigriti.com/profile/$1" | grep "streak" | cut -d'>' -f193 | sort | awk  '{print "[-] My streak Intigriti.com: "  $0}'| sed '2d'  | sed '3d' | sed '1d' | sort -u | uniq -u 
+
+
+#RankALL()
+
+curl -s "https://app.intigriti.com/profile/$1" | grep "Rank"| awk -F# '{print "[-] My Rank ALL TIME in Intigriti.com: "  $2}' | cut -d'<' -f1
+
+#RankDAYS()
+
+curl -s "https://app.intigriti.com/leaderboard?ninetydays=true&severity=1" | egrep "/profile/$1"  | cut -d '"' -f9 | cut -d '<' -f1 | cut -d '>' -f2 | awk -F# '{print "[-] My Rank 90 DAYS in Intigriti.com: "  $0}'
 
 
 #days()
